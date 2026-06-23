f86a2f06daf8b661c44930d9acaa633501ba5948
flow11928536-dev <SEUEMAILDOGITHUB>
2026-06-22 23:08:41 -0300

    adicionado-analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,18 +68,18 @@ export default function RootLayout({
       <head>
         {/* Google Analytics */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-ZGBD01Y9KP"
-          strategy="afterInteractive"
-        />
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZGBD01Y9KP"
+        strategy="beforeInteractive"
+      />
 
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-ZGBD01Y9KP');
-          `}
-        </Script>
+      <Script id="google-analytics" strategy="beforeInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZGBD01Y9KP');
+        `}
+      </Script>
 
         {/* JSON-LD Organization */}
         <script
