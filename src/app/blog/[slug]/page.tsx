d2039dcb87a12917d58bcb0e85636aac8b2e1d12
flow11928536-dev <SEUEMAILDOGITHUB>
2026-06-23 14:09:27 -0300

    ajustei o pagedoslug

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,10 +1,64 @@
-import { blogPosts } from '@/lib/blog-data'
+import { blogPosts, getBlogPostBySlug } from '@/lib/blog-data'
 import BlogPostPageClient from './BlogPostPageClient'
 
 export function generateStaticParams() {
   return blogPosts.map((post) => ({ slug: post.slug }))
 }
 
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+  const post = getBlogPostBySlug(params.slug) || blogPosts[0]
+  return {
+    title: post.title,
+    description: post.excerpt,
+    keywords: post.keywords,
+    openGraph: {
+      title: post.title,
+      description: post.excerpt,
+      type: 'article',
+      publishedTime: post.date,
+      authors: [post.author],
+      images: [post.image],
+    },
+  }
+}
+
 export default function BlogPostPage({ params }: { params: { slug: string } }) {
-  return <BlogPostPageClient slug={params.slug} />
+  const post = getBlogPostBySlug(params.slug) || blogPosts[0]
+
+  const schema = {
+    '@context': 'https://schema.org',
+    '@type': 'Article',
+    headline: post.title,
+    description: post.excerpt,
+    image: post.image,
+    author: {
+      '@type': 'Person',
+      name: post.author,
+    },
+    publisher: {
+      '@type': 'Organization',
+      name: 'Suplex Suplementos',
+      logo: {
+        '@type': 'ImageObject',
+        url: 'https://suplex-suplementos.pages.dev/logo.png',
+      },
+    },
+    datePublished: post.date,
+    dateModified: post.date,
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': `https://suplex-suplementos.pages.dev/blog/${post.slug}`,
+    },
+    keywords: post.keywords?.join(', '),
+  }
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      />
+      <BlogPostPageClient slug={params.slug} />
+    </>
+  )
 }
